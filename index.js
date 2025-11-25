96bcd6a4d810b46cc99d93e9f69cbc9258505941
nethsaraPrabash <prabash0393@gmail.com>
2025-11-25 15:04:21 +0530

    practicum done commit

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const axios = require('axios');
 const app = express();
+require('dotenv').config();
+
+// Middleware
+app.set('views', __dirname + '/views'); 
 
 app.set('view engine', 'pug');
 app.use(express.static(__dirname + '/public'));
@@ -10,17 +14,80 @@ app.use(express.json());
 // * Please DO NOT INCLUDE the private app access token in your repo. Don't do this practicum in your normal account.
 const PRIVATE_APP_ACCESS = process.env.PRIVATE_APP_ACCESS;
 
+
+
+const config = {
+    headers: {
+        Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
+        'Content-Type': 'application/json'
+    }
+}
+
 // TODO: ROUTE 1 - Create a new app.get route for the homepage to call your custom object data. Pass this data along to the front-end and create a new pug template in the views folder.
+app.get('/', async (_req, res) => {
+
+    try {
+        const response = await axios.get(
+            'https://api.hubapi.com/crm/v3/objects/p_cars?limit=10&properties=car_name,country,car_model&archived=false',
+            config
+        );
+
+        const cars = response.data.results.map(car => ({
+            name: car.properties.car_name,
+            model: car.properties.car_model,
+            country: car.properties.country
+        }));
+
+        res.render('homepage', { cars });
+
+    } catch (error) {
+        console.error('Error fetching cars:', error.message);
+        res.send('Error fetching cars. Check server console.');
+    }
+});
+
+
+
 
 // * Code for Route 1 goes here
 
 // TODO: ROUTE 2 - Create a new app.get route for the form to create or update new custom object data. Send this data along in the next route.
 
+app.get('/update-cobj', (_req, res) => {
+  res.render('updates');
+});
+
+
+
+
+
 // * Code for Route 2 goes here
 
 // TODO: ROUTE 3 - Create a new app.post route for the custom objects form to create or update your custom object data. Once executed, redirect the user to the homepage.
 
-// * Code for Route 3 goes here
+app.post('/update-cobj', async (req, res) => {
+  try {
+    const { car_name, car_model, country } = req.body;
+
+    const body = {
+      properties: {
+        car_name: car_name || '',
+        car_model: car_model || '',
+        country: country || ''
+      }
+    };
+
+    const createUrl = 'https://api.hubapi.com/crm/v3/objects/p_cars';
+
+    await axios.post(createUrl, body, config);
+
+    res.redirect('/');
+  } catch (err) {
+    console.error('Error creating car:', err.response?.data || err.message);
+    res.status(500).send('Error creating record. Check server console for details.');
+  }
+});
+
 
 /** 
 * * This is sample code to give you a reference for how you should structure your calls. 
@@ -41,8 +108,8 @@ app.get('/contacts', async (req, res) => {
     }
 });
 
-* * App.post sample
-app.post('/update', async (req, res) => {
+* * App.poste',  sample
+app.post('/updatasync (req, res) => {
     const update = {
         properties: {
             "favorite_book": req.body.newVal
@@ -65,7 +132,7 @@ app.post('/update', async (req, res) => {
 
 });
 */
-
+    ``
 
 // * Localhost
 app.listen(3000, () => console.log('Listening on http://localhost:3000'));
